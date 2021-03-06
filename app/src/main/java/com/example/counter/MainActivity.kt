c1eb33aa7988421a79a87ad38fe0decf34a7459f
Sairam Vankayalapati <svankayalapati@insulet.com>
2021-03-06 14:44:58 -0500

    Adding functionality for Click button

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private var userInputText: EditText? = null
     private var textView: TextView? = null
     private var clickButton: Button? = null
+    private var clicksCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +23,11 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.textView)
         clickButton = findViewById(R.id.clickButton)
 
+        clickButton?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                clicksCount += 1
+                textView?.append("\n # of times Button Clicked: $clicksCount")
+            }
+        })
     }
 }
