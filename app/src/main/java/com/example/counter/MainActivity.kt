0bd5dbc15f45766261eae1b5405104ccc6d27eb1
Sairam Vankayalapati <svankayalapati@insulet.com>
2021-03-06 14:04:24 -0500

    Initial commit of Counter App

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var userInputText: EditText? = null
+    private var textView: TextView? = null
+    private var clickButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInputText = findViewById(R.id.editText)
+        textView = findViewById(R.id.textView)
+        clickButton = findViewById(R.id.clickButton)
+
     }
 }
